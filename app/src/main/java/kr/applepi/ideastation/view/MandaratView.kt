199fd136f04b2f97fb98d597d336d8a16682debf
HealGaren <qkswkd7003@naver.com>
2016-09-28 21:12:16 +0900

    만다라트뷰에 전체보기 추가(중)

--- app/src/main/java/kr/applepi/ideastation/view/MandaratView.kt
+++ app/src/main/java/kr/applepi/ideastation/view/MandaratView.kt
@@ -18,6 +18,7 @@ import android.view.animation.Transformation
 import android.view.animation.TranslateAnimation
 import android.widget.*
 import kotlinx.android.synthetic.main.tile_mandarat.view.*
+import kotlinx.android.synthetic.main.tile_mandarat_show.view.*
 import kotlinx.android.synthetic.main.view_mandarat.view.*
 import kr.applepi.ideastation.R
 
@@ -37,7 +38,16 @@ class MandaratView : RelativeLayout {
     constructor(context: Context?, attrs: AttributeSet?)
     : this(context, attrs, 0)
 
-    val pref: SharedPreferences
+    private val pref: SharedPreferences
+
+    private var showAllMode: Boolean = false
+        set(value) {
+            if (!showAllMode && value) {
+
+            } else if (showAllMode && !value) {
+
+            }
+        }
 
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int)
     : super(context, attrs, defStyleAttr) {
@@ -47,87 +57,112 @@ class MandaratView : RelativeLayout {
 
         typedArray.recycle()
 
-        pref = context.getSharedPreferences(PREF_KEY, Context::MODE_PRIVATE.get())
+        pref = context.getSharedPreferences(PREF_KEY, Context.MODE_PRIVATE)
 
         LayoutInflater.from(context).inflate(R.layout.view_mandarat, this)
 
         setTextColor(textColor)
-        val tileArr: Array<View> = arrayOf(tile1, tile2, tile3, tile4, tile6, tile7, tile8, tile9)
 
+        superView_edit.run {
+            val tileArr = arrayOf(tile1, tile2, tile3, tile4, tile6, tile7, tile8, tile9)
 
-        for (i in 0..tileArr.size - 1) {
-            tileArr[i].run {
+            for (i in 0..tileArr.size - 1) {
+                tileArr[i].run {
 
-                arrayOf(edit1, edit2, edit3, edit4, edit6, edit7, edit8, edit9).forEach {
-                    it.setBackgroundColor(ContextCompat.getColor(context, R.color.colorMandaratLow))
-                }
-                edit5.setBackgroundColor(ContextCompat.getColor(context, R.color.colorMandaratMedium))
+                    arrayOf(edit1, edit2, edit3, edit4, edit6, edit7, edit8, edit9).forEach {
+                        it.setBackgroundColor(ContextCompat.getColor(context, R.color.colorMandaratLow))
+                    }
+                    edit5.setBackgroundColor(ContextCompat.getColor(context, R.color.colorMandaratMedium))
 
-                val arr: Array<Button> = arrayOf(
-                        btn_right_down, btn_down, btn_left_down,
-                        btn_right, btn_left,
-                        btn_right_up, btn_up, btn_left_up
-                )
+                    val arr: Array<Button> = arrayOf(
+                            btn_right_down, btn_down, btn_left_down,
+                            btn_right, btn_left,
+                            btn_right_up, btn_up, btn_left_up
+                    )
 
-                for (j in 0..arr.size - 1) {
-                    if (i != j) arr[j].visibility = View.INVISIBLE
-                    arr[j].setOnClickListener {
-                        gotoParent()
+                    for (j in 0..arr.size - 1) {
+                        if (i != j) arr[j].visibility = View.INVISIBLE
+                        arr[j].setOnClickListener {
+                            gotoParent()
+                        }
                     }
                 }
             }
+
+            tile5.run {
+
+                val editArr = arrayOf(edit1, edit2, edit3, edit4, edit6, edit7, edit8, edit9)
+                for (i in 0..tileArr.size - 1) {
+                    editArr[i].setBackgroundColor(ContextCompat.getColor(context, R.color.colorMandaratMedium))
+                    editArr[i].addTextChangedListener(object : TextWatcher {
+                        override fun afterTextChanged(p0: Editable?) {
+                        }
+
+                        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                        }
+
+                        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                            tileArr[i].edit5.setText(p0)
+                        }
+                    })
+
+                }
+                edit5.setBackgroundColor(ContextCompat.getColor(context, R.color.colorMandaratHigh))
+
+
+                val btnArr = arrayOf(btn_left_up, btn_up, btn_right_up,
+                        btn_left, btn_right,
+                        btn_left_down, btn_down, btn_right_down
+                )
+                for (i in 0..btnArr.size - 1) {
+                    btnArr[i].setOnClickListener { gotoChild(tileArr[i]) }
+                }
+            }
+            setTileToCenter()
         }
 
-        tile5.run {
+        val edits = superView_edit.run { arrayOf(tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9) }
+        val texts = superView_show.run { arrayOf(tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9) }
 
-            val editArr = arrayOf(edit1, edit2, edit3, edit4, edit6, edit7, edit8, edit9)
-            for(i in 0..tileArr.size-1) {
-                editArr[i].setBackgroundColor(ContextCompat.getColor(context, R.color.colorMandaratMedium))
-                editArr[i].addTextChangedListener(object : TextWatcher {
-                    override fun afterTextChanged(p0: Editable?) { }
-                    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+        for(i in 0..edits.size - 1){
+            val editTexts = edits[i].run{ arrayOf(edit1, edit2, edit3, edit4, edit5, edit6, edit7, edit8, edit9) }
+            val textViews = texts[i].run{ arrayOf(text1, text2, text3, text4, text5, text6, text7, text8, text9) }
+            for(j in 0..editTexts.size - 1){
+                editTexts[j].addTextChangedListener(object : TextWatcher{
+                    override fun afterTextChanged(p0: Editable?) {
+                    }
+
+                    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    }
 
                     override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                        tileArr[i].edit5.setText(p0)
+                        textViews[j].text = p0
                     }
                 })
-
-            }
-            edit5.setBackgroundColor(ContextCompat.getColor(context, R.color.colorMandaratHigh))
-
-
-            val btnArr = arrayOf(btn_left_up, btn_up, btn_right_up,
-                    btn_left, btn_right,
-                    btn_left_down, btn_down, btn_right_down
-            )
-            for (i in 0..btnArr.size - 1) {
-                btnArr[i].setOnClickListener { gotoChild(tileArr[i]) }
             }
         }
-
-        setTileToCenter()
-
 
     }
 
     private fun gotoParent() {
 
-        val ani = object : Animation() {
+        superView_edit.run {
+            val ani = object : Animation() {
 
-            val params: FrameLayout.LayoutParams = superView.layoutParams as FrameLayout.LayoutParams
-            val startLeft = params.leftMargin
-            val targetLeftBy = -(tile4.width + params.leftMargin)
-            val startTop = params.topMargin
-            val targetTopBy = -(tile2.height + params.topMargin)
+                val params: FrameLayout.LayoutParams = layoutParams as FrameLayout.LayoutParams
+                val startLeft = params.leftMargin
+                val targetLeftBy = -(tile4.width + params.leftMargin)
+                val startTop = params.topMargin
+                val targetTopBy = -(tile2.height + params.topMargin)
 
-            override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
-                params.leftMargin = startLeft + (targetLeftBy * interpolatedTime).toInt()
-                params.topMargin = startTop + (targetTopBy * interpolatedTime).toInt()
-                superView.layoutParams = params
+                override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+                    params.leftMargin = startLeft + (targetLeftBy * interpolatedTime).toInt()
+                    params.topMargin = startTop + (targetTopBy * interpolatedTime).toInt()
+                    layoutParams = params
+                }
             }
-        }
 //        ani.isFillEnabled = true // 애니메이션 후 이동한좌표에
-        ani.duration = 500 //지속시간
+            ani.duration = 500 //지속시간
 //        ani.setAnimationListener(object : Animation.AnimationListener {
 //            override fun onAnimationRepeat(animation: Animation?) {
 //            }
@@ -142,42 +177,44 @@ class MandaratView : RelativeLayout {
 //            }
 //        })
 
-        superView.startAnimation(ani)
+            startAnimation(ani)
+        }
     }
 
 
     private fun gotoChild(view: View) {
 
+        superView_edit.run {
 
-        val w: Float = when (view) {
-            tile1, tile4, tile7 -> 0
-            tile2, tile5, tile8 -> tile4.width
-            else -> tile4.width + tile5.width
-        }.toFloat()
+            val w: Float = when (view) {
+                tile1, tile4, tile7 -> 0
+                tile2, tile5, tile8 -> tile4.width
+                else -> tile4.width + tile5.width
+            }.toFloat()
 
-        val h: Float = when (view) {
-            tile1, tile2, tile3 -> 0
-            tile4, tile5, tile6 -> tile2.height
-            else -> tile2.height + tile5.height
-        }.toFloat()
+            val h: Float = when (view) {
+                tile1, tile2, tile3 -> 0
+                tile4, tile5, tile6 -> tile2.height
+                else -> tile2.height + tile5.height
+            }.toFloat()
 
-        val ani = object : Animation() {
+            val ani = object : Animation() {
 
-            val params: FrameLayout.LayoutParams = superView.layoutParams as FrameLayout.LayoutParams
-            val startLeft = params.leftMargin
-            val targetLeftBy = -(w + params.leftMargin)
-            val startTop = params.topMargin
-            val targetTopBy = -(h + params.topMargin)
+                val params: FrameLayout.LayoutParams = layoutParams as FrameLayout.LayoutParams
+                val startLeft = params.leftMargin
+                val targetLeftBy = -(w + params.leftMargin)
+                val startTop = params.topMargin
+                val targetTopBy = -(h + params.topMargin)
 
-            override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
-                params.leftMargin = startLeft + (targetLeftBy * interpolatedTime).toInt()
-                params.topMargin = startTop + (targetTopBy * interpolatedTime).toInt()
-                superView.layoutParams = params
+                override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+                    params.leftMargin = startLeft + (targetLeftBy * interpolatedTime).toInt()
+                    params.topMargin = startTop + (targetTopBy * interpolatedTime).toInt()
+                    layoutParams = params
+                }
             }
-        }
 
 //        ani.isFillEnabled = true // 애니메이션 후 이동한좌표에
-        ani.duration = 500 //지속시간
+            ani.duration = 500 //지속시간
 //        ani.setAnimationListener(object : Animation.AnimationListener {
 //            override fun onAnimationRepeat(animation: Animation?) {
 //            }
@@ -194,18 +231,15 @@ class MandaratView : RelativeLayout {
 //        })
 
 
-        superView.startAnimation(ani)
+            startAnimation(ani)
 
-
+        }
     }
-
 
     fun save() {
         arrayOf(tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9).forEach {
             it.run {
-
                 arrayOf(edit1, edit2, edit3, edit4, edit5, edit6, edit7, edit8, edit9).forEach {
-
 
 
                 }
@@ -214,20 +248,23 @@ class MandaratView : RelativeLayout {
     }
 
     fun setTextColor(color: Int) {
-        arrayOf(tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9).forEach {
-            it.run {
-                arrayOf(edit1, edit2, edit3, edit4, edit5, edit6, edit7, edit8, edit9).forEach {
-                    it.setTextColor(color)
+        superView_edit.run {
+            arrayOf(tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9).forEach {
+                it.run {
+                    arrayOf(edit1, edit2, edit3, edit4, edit5, edit6, edit7, edit8, edit9).forEach {
+                        it.setTextColor(color)
+                    }
                 }
             }
         }
     }
 
-    fun setTileToCenter(){
-        val params = superView.layoutParams as FrameLayout.LayoutParams
-        params.leftMargin = -tile4.width
-        params.topMargin = -tile2.height
-        superView.layoutParams = params
+    fun setTileToCenter() {
+        superView_edit.run {
+            val params = layoutParams as FrameLayout.LayoutParams
+            params.leftMargin = -tile4.width
+            params.topMargin = -tile2.height
+            layoutParams = params
+        }
     }
-
 }
